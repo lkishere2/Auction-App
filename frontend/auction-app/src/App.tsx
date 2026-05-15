9acc51438ddf7028eff3dbbd527715d8e94e25cd
LKLD1909 <codeboy1909@gmail.com>
2026-05-15 18:25:53 +0700

    Install some dependencies for FE

---
+++ frontend/auction-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-black dark:bg-slate-950 dark:text-white">
+
+    </div>
+  )
+}
+
+export default App
